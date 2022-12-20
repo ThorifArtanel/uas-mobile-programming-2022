49e1e42c509181f4ca5c5ea07d6dbf86d72c4ba6
Thorif Artanel <thorif.m.artanel@gmail.com>
2022-12-20 17:31:29 +0700

    Commit 2

--- app/src/main/java/edu/upi/cs/yudiwbs/uas_template/FragmentDua.java
+++ app/src/main/java/edu/upi/cs/yudiwbs/uas_template/FragmentDua.java
@@ -1,27 +1,42 @@
 package edu.upi.cs.yudiwbs.uas_template;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import edu.upi.cs.yudiwbs.uas_template.databinding.FragmentDuaBinding;
-import edu.upi.cs.yudiwbs.uas_template.databinding.FragmentSatuBinding;
 
-public class FragmentDua extends Fragment {
+public class FragmentDua extends Fragment implements SensorEventListener {
 
     private FragmentDuaBinding binding;
+    private SensorManager sm;
+    private Sensor senAccel;
+    private boolean isLifted;
 
-    ArrayList<Hasil> alHasil = new ArrayList<>();
-    AdapterHasil adapter;
+
+    ArrayList<Accel> alAccel = new ArrayList<>();
+    AdapterAccel adapter;
     RecyclerView.LayoutManager lm;
 
 
@@ -39,6 +54,19 @@ public class FragmentDua extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String TAG = "debug_yudi";
+
+        //cek apakah sensor tersedia
+        sm = (SensorManager) getActivity().getSystemService(getActivity().getApplicationContext().SENSOR_SERVICE);
+        senAccel = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (senAccel != null){
+            Log.d(TAG,"Sukses, device punya sensor accelerometer!");
+        }
+        else {
+            // gagal, tidak ada sensor accelerometer.
+            Log.d(TAG,"Tidak ada sensor accelerometer!");
+        }
+
     }
 
     @Override
@@ -47,7 +75,7 @@ public class FragmentDua extends Fragment {
         binding = FragmentDuaBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        adapter = new AdapterHasil(alHasil);
+        adapter = new AdapterAccel(alAccel);
         binding.rvHasil.setAdapter(adapter);
 
         lm = new LinearLayoutManager(getActivity());
@@ -59,10 +87,64 @@ public class FragmentDua extends Fragment {
         binding.buttonFrag2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                alHasil.add(new Hasil("satu..."));
+                SimpleDateFormat s = new SimpleDateFormat("hh:mm:ss");
+                String ts = s.format(new Date());
+                alAccel.add(new Accel(ts, "HP diangkat!"));
                 adapter.notifyDataSetChanged();
+
             }
         });
         return view;
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        double ax=0,ay=0,az=0;
+        // menangkap perubahan nilai sensor
+        if (sensorEvent.sensor.getType()==Sensor.TYPE_ACCELEROMETER) {
+            ax=sensorEvent.values[0];
+            ay=sensorEvent.values[1];
+            az=sensorEvent.values[2];
+        }
+
+
+        if  (ay>=2) {
+            isLifted = true;
+        }
+        if (isLifted) {
+            isLifted = false;
+            SimpleDateFormat s = new SimpleDateFormat("hh:mm:ss");
+            String ts = s.format(new Date());
+            alAccel.add(new Accel(ts, "HP diangkat!"));
+            adapter.notifyDataSetChanged();
+            isLifted = false;
+        } else{
+            //ax,ay,az bisa digunakan
+            long timestamp = System.currentTimeMillis();
+            // Menampilkan log dari accelerometer beserta timestamp
+            String msg = "X: " + ax + ", Y: " + ay + ", Z: " + az + ", Timestamp: " + timestamp;
+            Log.d("debug_yudi",msg);
+            binding.tvKeteranganFrag2.setText(msg);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sm.registerListener(this, senAccel, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
 }
--- app/src/main/java/edu/upi/cs/yudiwbs/uas_template/ViewModelFragmentSatu.java
+++ app/src/main/java/edu/upi/cs/yudiwbs/uas_template/ViewModelFragmentSatu.java
@@ -1,8 +1,21 @@
 package edu.upi.cs.yudiwbs.uas_template;
 
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+
 public class ViewModelFragmentSatu extends ViewModel {
-    public MutableLiveData<String> data;
+    public MutableLiveData<ArrayList<Advice>> dataAdvice;
+
+    public void setData(ArrayList<Advice> data) {
+        this.dataAdvice.setValue(data);
+    }
+
+    public ViewModelFragmentSatu() {
+        //inisialisasi
+        this.dataAdvice = new MutableLiveData<ArrayList<Advice>>();
+    }
+
 }
